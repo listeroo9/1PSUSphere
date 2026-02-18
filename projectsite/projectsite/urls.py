18b8f6d48d22c127e6ee950f197649df820656b4
John Erwin Anquillano <listeroo92004@gmail.com>
2026-02-18 04:16:36 +0000

    add views.py and edit urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import home  # import your home view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # map the root URL to your home view
 ]
